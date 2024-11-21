903f75cc571ec16a4483af4939bf762aee4dccea
shinecoop <coop.shine0213@gmail.com>
2024-11-20 21:21:13 -0700

    funky distribution, updated verification, clean up

--- metropolis.py
+++ metropolis.py
@@ -4,13 +4,11 @@ from scipy.stats import gaussian_kde as gkde
 from scipy.stats import norm, uniform, loggamma
 
 def f(x):
-    if x >= 1:
-        return np.exp(-(x-1)/2) + np.exp(-(x-1)**2)
-    else:
-        return np.exp((x-1)/3) + np.exp((x-1)**3)
+
+    return np.abs((np.sin(5*x) + np.cos(3*x) + (1/(1+x**2))) * np.exp(-x**4))
 
 def generate_candidate(x):
-    return np.random.normal(x,1)    
+    return np.random.normal(x,1) #generate candidate from normal distribution centered around current
 
 def generate_acceptance(x,x1):
     rf = f(x1)/f(x)
@@ -19,9 +17,8 @@ def generate_acceptance(x,x1):
     return min(1,rf)
 
 def change_states(acceptance):
-    threshold = np.random.uniform(0,1)
-
-    if acceptance>threshold:
+    
+    if acceptance>=np.random.uniform(0,1):
         return True
     return False
 
@@ -65,25 +62,27 @@ def main():
     y_vals = [f(x) for x in x_vals] #f values
 
     #values used for verification in the future 
-    NORM_CONST = 7.16556
-    TRUE_MEAN = 1.94709 / NORM_CONST
+    NORM_CONST=2.12218
+    TRUE_MEAN = 0.34085/ NORM_CONST
 
     p_vals = [f(x)/NORM_CONST for x in x_vals] #pi values
-
+    
+    #plotting 
     plt.plot(x_vals, y_vals, color="red")
     plt.plot(x_vals, p_vals, color="blue")
-    
+
+     
     #plot setup
     plt.xlabel('x', fontsize=20)
     plt.ylabel('density', fontsize=20)
     plt.legend(['f(x)','PI(x)'], fontsize=20)
+    plt.xlim(-10,10)
 
-    #metropolis that hoe
+    #metropolis
     met=metropolis(samples=100000)
-    plt.hist(met, bins=200, density=True)
-
+    plt.hist(met, bins=200, density=True, color="orange")
     #verification
-    plt.title(f"Expected Mean: {round(TRUE_MEAN,3)} \n Mean We Got:{round(np.mean(met),3)}")
+    plt.title(f"Expected Mean: {round(TRUE_MEAN,3)} \n Mean We Got: {round(np.mean(met),3)}")
 
 
     plt.show()
